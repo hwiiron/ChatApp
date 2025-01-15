8ee609f81d8c09aef8320bb7b209a5280dc88445
HwichulShin <shinhwiiron@gmail.com>
2025-01-15 10:43:52 +0900

    mongoose 스키마 주석 설명 추가

--- Models/user.js
+++ Models/user.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {
+    // 유저 이름
     type: String,
     require: [true, "User must type name"],
     unique: true,
   },
   token: {
+    // 유저 고유 토큰
     type: String,
   },
   online: {
+    // 유저 접속 상태
     type: Boolean,
     default: false,
   },
