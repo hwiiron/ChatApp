0d896b174902682014d566204d18a9b5afe56f74
HwichulShin <shinhwiiron@gmail.com>
2025-01-15 10:30:34 +0900

    mongoose 스키마 생성

---
+++ Models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "User must type name"],
+    unique: true,
+  },
+  token: {
+    type: String,
+  },
+  online: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
