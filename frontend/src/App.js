e5eae0d512168c2d4a469e154d8f389a2f6fca1e
HwichulShin <shinhwiiron@gmail.com>
2025-01-16 11:18:47 +0900

    양방향 실시간 메시지 전송 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,47 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import socket from "./server";
+import InputField from "./components/InputField/InputField";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      console.log("res", message);
+    });
+    askUserName();
+  }, []);
+
+  const askUserName = () => {
+    const userName = prompt("이름을 입력해 주세요.");
+    console.log(userName);
+
+    socket.emit("login", userName, (res) => {
+      console.log("res", res);
+      if (res?.ok) {
+        setUser(res.data);
+      }
+    });
+  };
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit("sendMessage", message, (res) => {
+      console.log("sendMessage res", res);
+    });
+  };
+
   return (
     <div>
-      <div className="App"></div>
+      <div className="App">
+        <InputField
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
     </div>
   );
 }
